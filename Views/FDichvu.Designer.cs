a3aaebe829afdfdfa2e6766e371001a8d703b9cd
2374801030072 - Đào Đức Trung - 71K29KTPM02 <trung.2374801030072@vanlanguni.vn>
2024-07-24 21:51:28 +0700

    update form dich vu

--- Views/FDichvu.Designer.cs
+++ Views/FDichvu.Designer.cs
@@ -50,11 +50,12 @@
             // lsvDanhSachDichVu
             // 
             this.lsvDanhSachDichVu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvDanhSachDichVu.Font = new System.Drawing.Font("Cambria", 14.25F);
             this.lsvDanhSachDichVu.HideSelection = false;
-            this.lsvDanhSachDichVu.Location = new System.Drawing.Point(2, 23);
-            this.lsvDanhSachDichVu.Margin = new System.Windows.Forms.Padding(2);
+            this.lsvDanhSachDichVu.Location = new System.Drawing.Point(3, 25);
+            this.lsvDanhSachDichVu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lsvDanhSachDichVu.Name = "lsvDanhSachDichVu";
-            this.lsvDanhSachDichVu.Size = new System.Drawing.Size(873, 240);
+            this.lsvDanhSachDichVu.Size = new System.Drawing.Size(1163, 299);
             this.lsvDanhSachDichVu.TabIndex = 0;
             this.lsvDanhSachDichVu.UseCompatibleStateImageBehavior = false;
             this.lsvDanhSachDichVu.SelectedIndexChanged += new System.EventHandler(this.lsvDanhSachDichVu_SelectedIndexChanged_1);
@@ -62,12 +63,13 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.lsvDanhSachDichVu);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(9, 279);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox2.Location = new System.Drawing.Point(108, 401);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(877, 265);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(1169, 326);
             this.groupBox2.TabIndex = 19;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Danh sách dịch vụ";
@@ -75,94 +77,101 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(56, 133);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.label4.Location = new System.Drawing.Point(105, 198);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 24);
+            this.label4.Size = new System.Drawing.Size(71, 22);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Giá tiền:";
+            this.label4.Text = "Giá tiền";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(415, 64);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.label3.Location = new System.Drawing.Point(595, 113);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 24);
+            this.label3.Size = new System.Drawing.Size(102, 22);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Tên dịch vụ : ";
+            this.label3.Text = "Tên dịch vụ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 64);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.label2.Location = new System.Drawing.Point(83, 113);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 24);
+            this.label2.Size = new System.Drawing.Size(94, 22);
             this.label2.TabIndex = 4;
-            this.label2.Text = "ID Dịch vụ:";
+            this.label2.Text = "ID Dịch vụ";
             // 
             // txtGiatien
             // 
-            this.txtGiatien.Location = new System.Drawing.Point(166, 131);
-            this.txtGiatien.Margin = new System.Windows.Forms.Padding(2);
+            this.txtGiatien.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.txtGiatien.Location = new System.Drawing.Point(237, 195);
+            this.txtGiatien.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtGiatien.Name = "txtGiatien";
-            this.txtGiatien.Size = new System.Drawing.Size(185, 28);
+            this.txtGiatien.Size = new System.Drawing.Size(245, 30);
             this.txtGiatien.TabIndex = 2;
             this.txtGiatien.TextChanged += new System.EventHandler(this.txtGiatien_TextChanged);
             // 
             // txtTendichvu
             // 
-            this.txtTendichvu.Location = new System.Drawing.Point(563, 63);
-            this.txtTendichvu.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTendichvu.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.txtTendichvu.Location = new System.Drawing.Point(767, 112);
+            this.txtTendichvu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTendichvu.Name = "txtTendichvu";
-            this.txtTendichvu.Size = new System.Drawing.Size(185, 28);
+            this.txtTendichvu.Size = new System.Drawing.Size(245, 30);
             this.txtTendichvu.TabIndex = 1;
             this.txtTendichvu.TextChanged += new System.EventHandler(this.txtTendichvu_TextChanged);
             // 
             // txtIDDichvu
             // 
-            this.txtIDDichvu.Location = new System.Drawing.Point(166, 62);
-            this.txtIDDichvu.Margin = new System.Windows.Forms.Padding(2);
+            this.txtIDDichvu.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.txtIDDichvu.Location = new System.Drawing.Point(237, 110);
+            this.txtIDDichvu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtIDDichvu.Name = "txtIDDichvu";
-            this.txtIDDichvu.Size = new System.Drawing.Size(185, 28);
+            this.txtIDDichvu.Size = new System.Drawing.Size(245, 30);
             this.txtIDDichvu.TabIndex = 0;
             this.txtIDDichvu.TextChanged += new System.EventHandler(this.txtIDDichvu_TextChanged);
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txtGiatien);
             this.groupBox1.Controls.Add(this.txtTendichvu);
             this.groupBox1.Controls.Add(this.txtIDDichvu);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(9, 9);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Location = new System.Drawing.Point(108, 69);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(877, 198);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1169, 328);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin dịch vụ";
             // 
             // btnNhapMoi
             // 
-            this.btnNhapMoi.Location = new System.Drawing.Point(32, 163);
-            this.btnNhapMoi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNhapMoi.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.btnNhapMoi.Location = new System.Drawing.Point(48, 244);
+            this.btnNhapMoi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnNhapMoi.Name = "btnNhapMoi";
-            this.btnNhapMoi.Size = new System.Drawing.Size(165, 35);
+            this.btnNhapMoi.Size = new System.Drawing.Size(220, 43);
             this.btnNhapMoi.TabIndex = 3;
             this.btnNhapMoi.Text = "Nhập mới";
             this.btnNhapMoi.UseVisualStyleBackColor = true;
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(32, 55);
-            this.btnXoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btnXoa.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.btnXoa.Location = new System.Drawing.Point(48, 119);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(165, 35);
+            this.btnXoa.Size = new System.Drawing.Size(220, 43);
             this.btnXoa.TabIndex = 2;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
@@ -170,10 +179,11 @@
             // 
             // btnCapNhat
             // 
-            this.btnCapNhat.Location = new System.Drawing.Point(32, 104);
-            this.btnCapNhat.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCapNhat.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.btnCapNhat.Location = new System.Drawing.Point(48, 179);
+            this.btnCapNhat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCapNhat.Name = "btnCapNhat";
-            this.btnCapNhat.Size = new System.Drawing.Size(165, 35);
+            this.btnCapNhat.Size = new System.Drawing.Size(220, 43);
             this.btnCapNhat.TabIndex = 1;
             this.btnCapNhat.Text = "Cập nhật";
             this.btnCapNhat.UseVisualStyleBackColor = true;
@@ -181,10 +191,11 @@
             // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(32, 11);
-            this.btnThem.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThem.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.btnThem.Location = new System.Drawing.Point(48, 57);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(165, 35);
+            this.btnThem.Size = new System.Drawing.Size(220, 43);
             this.btnThem.TabIndex = 0;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
@@ -192,26 +203,28 @@
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.Tan;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.panel2.Controls.Add(this.btnNhapMoi);
             this.panel2.Controls.Add(this.btnXoa);
             this.panel2.Controls.Add(this.btnCapNhat);
             this.panel2.Controls.Add(this.btnThem);
-            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel2.Location = new System.Drawing.Point(890, 9);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.panel2.Location = new System.Drawing.Point(1425, 69);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(230, 531);
+            this.panel2.Size = new System.Drawing.Size(307, 654);
             this.panel2.TabIndex = 17;
             // 
             // FDichvu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 553);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1819, 773);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FDichvu";
             this.Text = "FDichvu";
             this.Load += new System.EventHandler(this.FDichvu_Load_2);
